c85966d1108680a8789e896ca87167b58df01692
Shahzod <shahzod20030905@gmail.com>
2022-04-18 02:24:58 +0200

    Added routings and in addition created a pattern which will be used in almost every html document

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about-us', views.about,),
-    path('contact-us', views.contact),
-    path('services', views.services),
-    path('portfolio', views.portfolio),
+    path('', views.index, name ='home'),
+    path('about-us', views.about, name ='about' ),
+    path('contact-us', views.contact, name = 'contact'  ),
+    path('services', views.services, name = 'services'),
+    path('portfolio', views.portfolio, name = 'portfolio'),
 ]
