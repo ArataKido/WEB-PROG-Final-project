74904468b8e997ac6d441f2ac9dd83de798238ee
Shahzod <shahzod20030905@gmail.com>
2022-04-23 23:18:44 +0200

    now it is possible to save froms in Contact Us

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+import imp
+from django.shortcuts import redirect, render
 from .forms import ContactForm
-
+from .models import ContactUs
 def index(request):
     return render(request, "polls/index.html")
 
@@ -8,11 +9,29 @@ def about(request):
     return render(request, "polls/about.html")
 
 def contact(request):
-    form = ContactForm()
-    context={
-        'form': form 
-    }
-    return render(request, "polls/contact.html", context )
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            cd = form.cleaned_data
+            to_save = ContactUs(
+                name = cd["name"],
+                phone = cd["phone"],
+                email = cd["email"],
+                message = cd["message"],
+            )
+            to_save.save()
+            return redirect('home')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm()
+        context={
+            'form': form 
+        }
+        return render(request, "polls/contact.html", context )
 
 def services(request):
     return render(request, "polls/services.html")
